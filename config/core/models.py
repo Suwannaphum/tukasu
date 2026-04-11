aa9cc54635708ab733dcaad9e4d701cd963f6b14
Mhai-hplinux <suwannaphum.ins@gmail.com>
2026-04-11 15:32:58 +0700

    design models

---
+++ config/core/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    cappacity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Gallery(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
+    image = models.ImageField(upload_to='gallery/')
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.room.name if self.room else 'General'}"
+    
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=20)
+    line = models.CharField(max_length=100)
+    facebook = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+
+    def __str__(self):
+        return "Contact Info"
+    
